e9312253d934022efb8b53be027d44eceaceca39
freeman <iviewn@gmail.com>
2023-02-07 22:05:18 +0300

    index

--- js/custom.js
+++ js/custom.js
@@ -2,14 +2,17 @@ $(document).ready(function () {
     setTimeout(function() { 
         $('body').addClass('oh');
         $('.modal-bg').addClass('vis');
+        $('.modal').addClass('vis');
     }, 2000);
     $('.modal__close').click(function() {
         $('body').removeClass('oh');
         $('.modal-bg').removeClass('vis');
+        $('.modal').removeClass('vis');
     });
     function modal(){
         $('body').addClass('oh');
         $('.modal-bg').addClass('vis');
+        $('.modal').addClass('vis');
     }
     $('.user__action').click(modal);
 });
