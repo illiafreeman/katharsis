7fe8cdcfde153cfa70ad84e28856f180b2220060
freeman <iviewn@gmail.com>
2023-02-08 20:28:56 +0300

    index

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    /*setTimeout(function() { 
+    setTimeout(function() { 
          $('body').addClass('oh');
          $('.modal-bg').addClass('vis');
          $('.modal').addClass('vis');
@@ -18,7 +18,7 @@ $(document).ready(function () {
     $('.post__more').click(function() {
         $(this).parent().addClass('full');
         $(this).remove();
-    });*/
+    });
 
     let elem = document.querySelector('.post__all');
     elem.addEventListener('click', function(event) {
