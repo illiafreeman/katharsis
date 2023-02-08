d48e9d10730ec4a5bc995f12edf05046cc54afe0
freeman <iviewn@gmail.com>
2023-02-08 16:59:35 +0300

    index

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    /*setTimeout(function() { 
+    setTimeout(function() { 
          $('body').addClass('oh');
          $('.modal-bg').addClass('vis');
          $('.modal').addClass('vis');
@@ -14,12 +14,12 @@ $(document).ready(function () {
         $('.modal-bg').addClass('vis');
         $('.modal').addClass('vis');
     }
-    $('.btn, .login, .post__btn, .post__all, .user__follow-inner, .post__head-action, .post__all').click(modal);*/
+    $('.btn, .login, .post__btn, .post__all, .user__follow-inner, .post__head-action, .post__all').click(modal);
     $('.post__more').click(function() {
         $(this).parent().addClass('full');
         $(this).remove();
     });
-    
+
     let elem = document.querySelector('.post__all');
     elem.addEventListener('click', function(event) {
         event.preventDefault();
